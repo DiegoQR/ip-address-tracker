9c53c6a37592b7ef70e79636b4a29f7e50dbbdae
DiegoQR <diegoqr2000@gmail.com>
2025-04-17 18:09:58 -0400

    Adjust spacing in InfoSection and update MapContainer height for improved layout

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -9,7 +9,7 @@ interface Props {
 function Map({latitude, longitude, zoom} : Props) {
 
     return (<>
-        <MapContainer center={[latitude, longitude]} zoom={zoom} scrollWheelZoom={true} className='w-full h-full absolute -z-50 top-75'>
+        <MapContainer center={[latitude, longitude]} zoom={zoom} scrollWheelZoom={true} className='w-full h-[60vh] absolute -z-50 top-75'>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
