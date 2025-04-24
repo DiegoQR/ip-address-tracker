cec27ee55f63f12ade089ab061fefbb5439bc460
DiegoQR <diegoqr2000@gmail.com>
2025-04-24 18:22:52 -0400

    Fix layout by adding padding to InfoSection component

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -3,7 +3,7 @@ import InfoDisplay from "./InfoDisplay";
 
 function InfoSection({ ipInfo }: { ipInfo: ipGeolocation }) {
     return (<>
-        <section className="bg-white rounded-2xl text-black py-7 space-y-3 shadow-xl">
+        <section className="bg-white rounded-2xl text-black py-7 space-y-3 shadow-xl px-5">
             <InfoDisplay label="IP ADDRESS">{ipInfo.ipAddress}</InfoDisplay>
             <InfoDisplay label="LOCATION">{ipInfo.location}</InfoDisplay>
             <InfoDisplay label="TIMEZONE">{ipInfo.timezone}</InfoDisplay>
